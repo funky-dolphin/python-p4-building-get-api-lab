92ff59917b90aaf6efbd2d716754e5f37141f883
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-04-04 17:24:58 -0400

    Lab complete

--- server/app.py
+++ server/app.py
@@ -20,19 +20,39 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict = bakery.to_dict()
+        bakeries.append(bakery_dict)
+    response = make_response(bakeries, 200)
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
+    response = make_response(jsonify(bakery_dict), 200)
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    goods = []
+    baked_list = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    for bakedgood in baked_list:
+        bakedgood_dict = bakedgood.to_dict()
+        goods.append(bakedgood_dict)
+    response = make_response(jsonify(goods), 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+  baked_list = BakedGood.query.order_by(BakedGood.price.desc()).first()
+  bakedgood_dict = baked_list.to_dict()
+  response = make_response(jsonify(bakedgood_dict), 200)
+  return response
 
 if __name__ == '__main__':
     app.run(port=555, debug=True)
+
+
+
--- server/models.py
+++ server/models.py
@@ -11,10 +11,25 @@ db = SQLAlchemy(metadata=metadata)
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
-    id = db.Column(db.Integer, primary_key=True)
+    serialize_rules = ('-baked_goods.bakery', )
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.Date)
+    updated_at = db.Column(db.Date)
+
+    baked_goods = db.relationship('BakedGood', backref = "bakery")
+
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
+    serialize_rules = ('-bakery.baked_goods', )
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.Date)
+    updated_at = db.Column(db.Date)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
     
